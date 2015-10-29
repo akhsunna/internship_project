7569d2339afbf4788c05eb2c1b51513db68f2535
akhsunna <akhsunna4@gmail.com>
2015-10-29 12:06:40 +0200

    add books

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029083517) do
+ActiveRecord::Schema.define(version: 20151029095041) do
+
+  create_table "books", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.string   "title",      limit: 255
+    t.string   "year",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "books", ["user_id"], name: "index_books_on_user_id", using: :btree
 
   create_table "identities", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
@@ -49,5 +59,6 @@ ActiveRecord::Schema.define(version: 20151029083517) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "books", "users"
   add_foreign_key "identities", "users"
 end
