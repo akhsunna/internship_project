0c1370f5b1ec0463b5eb7cc194c5418a7ce82c13
akhsunna <akhsunna4@gmail.com>
2015-10-30 11:18:43 +0200

    add edit book

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,8 +2,8 @@ class Book < ActiveRecord::Base
   belongs_to :user
   belongs_to :author
 
-  has_many :ganre_books, dependent: :delete_all
-  has_many :ganres, through: :ganre_books
+  # has_many :ganre_books, dependent: :delete_all
+  # has_many :ganres, through: :ganre_books
 
   belongs_to :language
 
--- app/models/ganre.rb
+++ app/models/ganre.rb
@@ -1,6 +1,6 @@
 class Ganre < ActiveRecord::Base
 
-  has_many :book_ganres, dependent: :delete_all
-  has_many :books, through: :book_ganres
+  # has_many :book_ganres, dependent: :delete_all
+  # has_many :books, through: :book_ganres
 
 end
