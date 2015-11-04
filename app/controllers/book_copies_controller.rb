a75d423f24a4c0a231b2256554443050eab7076c
akhsunna <akhsunna4@gmail.com>
2015-11-04 13:37:18 +0200

    removing copies

---
+++ app/controllers/book_copies_controller.rb
@@ -0,0 +1,15 @@
+class BookCopiesController < ApplicationController
+
+  def delete
+    @book_copy = BookCopy.find(params[:book_copy_id])
+    @book = @book_copy.book
+  end
+
+  def destroy
+    @book_copies = BookCopy.all
+    @book_copy = BookCopy.find(params[:id])
+    @book = @book_copy.book
+    @book_copy.destroy
+  end
+
+end
