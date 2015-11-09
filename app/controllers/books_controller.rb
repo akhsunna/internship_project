bfccbb7445d433a146f3d77a5f50dc88131e1fd9
akhsunna <akhsunna4@gmail.com>
2015-11-09 15:15:54 +0200

    fix

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all.order('updated_at DESC')
-    @authors = Author.all.reverse_each(&:readers)
+    @authors = Author.all.reverse_each(&:readers).first(8)
     @languages = Language.all
     @genres = Genre.all
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,10 @@ class UsersController < ApplicationController
     redirect_to moderator_path(current_user) if current_user.moderator?
     @user = User.find(params[:id])
 
-    @bookcopiesnow = BookCopyUser.where(user_id: current_user.id, return_date: nil)
+
+    @bookcopies = BookCopyUser.where(user_id: current_user.id)
+    @bookcopiesnow = @bookcopies.where(return_date: nil)
+    @bookcopiesnotnow = @bookcopies.where.not(return_date: nil)
   end
 
   def index
